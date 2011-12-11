e307ce0d5209686807f26b4111f82d2038dd993e
Charl van Niekerk <charl@ushahidi.com>
2011-12-11 19:52:49 +0100

    Added wireless driver scripts.

---
+++ airport2.sh
@@ -0,0 +1,6 @@
+# http://www.ubuntubuzz.com/2011/10/macbook-pro-wireless-broadcom-bcm4331.html
+sudo modprobe b43
+cp /etc/modules /etc/modules.old
+echo "b43" | sudo tee -a /etc/modules 
+cp /etc/pm/config.d/default /etc/pm/config.d/default.old
+echo 'SUSPEND_MODULES="b43"' >> /etc/pm/config.d/default
