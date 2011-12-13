7ee02b997cb7f56d81085f6fdf1aee676ffd4570
Charl van Niekerk <charl@ushahidi.com>
2011-12-13 11:10:08 +0100

    Update airport2.sh

--- airport2.sh
+++ airport2.sh
@@ -1,6 +1,3 @@
-# http://www.ubuntubuzz.com/2011/10/macbook-pro-wireless-broadcom-bcm4331.html
 modprobe b43
-cp /etc/modules /etc/modules.old
-echo "b43" | sudo tee -a /etc/modules 
-cp /etc/pm/config.d/default /etc/pm/config.d/default.old
+echo b43 >> /etc/modules 
 echo 'SUSPEND_MODULES="b43"' >> /etc/pm/config.d/default
