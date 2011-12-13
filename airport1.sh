ce7a74f7c107b1e0577ead4b58847c62a369fc33
Charl van Niekerk <charl@ushahidi.com>
2011-12-13 11:16:46 +0100

    Update airport1.sh

--- airport1.sh
+++ airport1.sh
@@ -1,4 +1,6 @@
 apt-get install build-essential automake make
+wget -c http://wireless.kernel.org/download/compat-wireless-2.6/compat-wireless-2011-08-27.tar.bz2
+wget -c -O patch_bcm4331.tar.bz2 http://ubuntuone.com/4Z8eqdGF1jcooqUoY4IsPN
 tar xf compat-wireless-2011-08-27.tar.bz2
 tar xf patch_bcm4331.tar.bz2
 cd compat-wireless-2011-08-27/
@@ -10,6 +12,8 @@ sed -i '/# CONFIG_B43_PHY_HT=y/cCONFIG_B43_PHY_HT=y' config.mk
 make
 make install
 cd ..
+wget -c http://bu3sch.de/b43/fwcutter/b43-fwcutter-015.tar.bz2
+wget -c http://www.lwfinger.com/b43-firmware/broadcom-wl-5.100.138.tar.bz2
 tar xf b43-fwcutter-015.tar.bz2
 cd b43-fwcutter-015
 make
