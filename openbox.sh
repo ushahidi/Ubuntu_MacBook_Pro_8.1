cad9072ac1cf8d7149153a7e4376758ed3d8b7c7
Charl van Niekerk <charl@ushahidi.com>
2012-01-02 12:33:02 +0100

    Reorganised and commented.

--- openbox.sh
+++ openbox.sh
@@ -13,22 +13,36 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Install Packages
 apt-get update
 apt-get install -y openbox feh gpomme tint2 xfce4-power-manager
 
+# Configure Keyboard Layout
 echo 'setxkbmap macintosh_vndr/us' >> /etc/xdg/openbox/autostart
+
+# Two-Finger Tap Perform Right Click
+echo 'synclient TapButton2=3' >> /etc/xdg/openbox/autostart
+
+# Disable Three-Finger Tap
+echo 'synclient TapButton3=0' >> /etc/xdg/openbox/autostart
+
+# Decrease Touchpad Sensitivity
+echo 'synclient FingerHigh=80' >> /etc/xdg/openbox/autostart
+
+# Display the Desktop Wallpaper
 echo 'feh --bg-max /home/user/wallpaper.jpg &' >> /etc/xdg/openbox/autostart
+
+# Visual Display for Apple Hotkeys
 echo 'gpomme &' >> /etc/xdg/openbox/autostart
+
+# Taskbar
 echo 'tint2 &' >> /etc/xdg/openbox/autostart
+
+# Power Manager Applet
 echo 'xfce4-power-manager &' >> /etc/xdg/openbox/autostart
 
+# Disable Multiple Desktops
 sed -i '/<number>/c<number>1</number>' /etc/xdg/openbox/rc.xml
+
+# Display Openbox Menu on Right Click in Taskbar
 sed -i '/wm_menu = 0/cwm_menu = 1' /etc/xdg/tint2/tint2rc
-
-sed -i '/EndSection/iOption "TabButton2" "3"' /usr/share/X11/xorg.conf.d/50-synaptics.conf
-sed -i '/EndSection/iOption "TabButton3" "0"' /usr/share/X11/xorg.conf.d/50-synaptics.conf
-sed -i '/EndSection/iOption "FingerHigh" "80"' /usr/share/X11/xorg.conf.d/50-synaptics.conf
-
-synclient TapButton2=3
-synclient TapButton3=0
-synclient FingerHigh=80
